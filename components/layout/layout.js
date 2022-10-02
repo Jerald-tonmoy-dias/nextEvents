9bcf5860d6f7570cc748440830a17d0b4426e71b
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2022-10-03 00:19:56 +0600

    navbar

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import MainHeader from "./main-header"
+
+export default function Layout(props) {
+    return (
+        <>
+            <MainHeader />
+            <main>
+                {props.children}
+            </main>
+        </>
+    )
+}
