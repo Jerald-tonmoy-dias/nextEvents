99fbe5f30211ea66434a76fff06bc3ad09933b04
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2022-10-03 00:26:04 +0600

    all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,10 @@
-
-
+import { getAllEvents } from "../../dummy-data"
+import EvetntList from '../../components/events/event-list'
 export default function AllEventsPage() {
+    const events = getAllEvents();
     return (
-        <div>all events page</div>
+        <div>
+            <EvetntList items={events} />
+        </div>
     )
 }
