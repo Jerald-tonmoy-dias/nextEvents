4c08bf1d82a654b6bd8bb00273d6922aedd8c452
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2022-10-05 22:33:04 +0600

    events page dynamice

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
 
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EvetntList from '../../components/events/event-list';
 import EventsSearch from "../../components/events/events-search";
 import { useRouter } from "next/router";
 
-export default function AllEventsPage() {
+export default function AllEventsPage(props) {
     const router = useRouter();
-    const events = getAllEvents();
+    const events = props.events;
 
     function findEventsHandler(year, month) {
         const fullPath = `/events/${year}/${month}`;
@@ -18,4 +18,15 @@ export default function AllEventsPage() {
             <EvetntList items={events} />
         </>
     )
+}
+
+export async function getStaticProps() {
+    const events = await getAllEvents();
+
+    return {
+        props: {
+            events: events
+        },
+        revalidate: 60
+    }
 }
